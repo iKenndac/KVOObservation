113a2875b6d91912ead1678037090c08fa17a7a6
Daniel Kennett <daniel@cascable.se>
2015-10-20 18:02:38 +0200

    Add OSS license, rename project

--- KVOObservationTests/KVOObservationTests.swift
+++ KVOObservationTests/KVOObservationTests.swift
@@ -1,13 +1,30 @@
-//
-//  CBLObservationTests.swift
-//  CBLObservationTests
-//
-//  Created by Daniel Kennett on 29/09/15.
-//  Copyright © 2015 Cascable AB. All rights reserved.
-//
+/*
+Copyright (c) 2015, Daniel Kennett
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the
+following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+following disclaimer in the documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote
+products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
 import XCTest
-@testable import CBLObservation
+@testable import KVOObservation
 
 class TestObject : NSObject {
     dynamic var name: String?
@@ -19,7 +36,7 @@ class TestObject : NSObject {
     }
 }
 
-class CBLObservationTests: XCTestCase {
+class KVOObservationTests: XCTestCase {
 
     func testInitial() {
 
